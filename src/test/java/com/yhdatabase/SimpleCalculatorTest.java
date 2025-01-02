bd74ab07fc6e6063800de5818778b6da9a10cc88
changmin5710 <im7kr@naver.com>
2025-01-02 17:41:02 +0900

    work

--- src/test/java/com/yhdatabase/SimpleCalculatorTest.java
+++ src/test/java/com/yhdatabase/SimpleCalculatorTest.java
@@ -9,10 +9,19 @@ public class SimpleCalculatorTest {
 
     @Test
     @DisplayName("1 + 1 = 3")
-    public void testPlus() {
+    public void t1() {
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         int rs = simpleCalculator.plus(1, 2);
 
         assertEquals(3, rs);
+    }
+
+    @Test
+    @DisplayName("2 + 10 = 12")
+    public void t2() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.plus(2, 10);
+
+        assertEquals(12, rs);
     }
 }
