8e8a1e754fe8f9b618220485e614cafef7c785e9
changmin5710 <im7kr@naver.com>
2025-01-02 17:33:48 +0900

    work

---
+++ src/test/java/com/yhdatabase/SimpleCalculatorTest.java
@@ -0,0 +1,18 @@
+package com.yhdatabase;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleCalculatorTest {
+
+    @Test
+    @DisplayName("1 + 1 = 3")
+    public void testPlus() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.plus(1, 2);
+
+        assertEquals(3, rs);
+    }
+}
